5b7eb761a9a13a298d05405f22f51ef587d83ed2
Hugo <hugo.lacourr@gmail.com>
2024-11-19 10:55:28 +0100

    Initial Commit

---
+++ GameOne/GameOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOneApp.swift
+//  GameOne
+//
+//  Created by Lacour Hugo on 19/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct GameOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GameOneTests/GameOneTests.swift
@@ -0,0 +1,17 @@
+//
+//  GameOneTests.swift
+//  GameOneTests
+//
+//  Created by Lacour Hugo on 19/11/2024.
+//
+
+import Testing
+@testable import GameOne
+
+struct GameOneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
